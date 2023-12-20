79e728a481e60984c093b679de4243d1b0b7c9b4
AshishRawat08 <rawashu18@gmail.com>
2023-12-20 16:21:34 +0530

    state and hooks

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,17 +3,15 @@ import ClassComponent from "./components/ClassComponent";
 import FunctionComponent from "./components/FunctionComponent";
 
 function App() {
+  const author = "ar";
   return (
     <div className="App">
       <p>Class Components</p>
       <ClassComponent />
       <br />
       <p>Function Components</p>
-      <FunctionComponent />  
-      
-      {/* can be call many time  as per need */}
-      <FunctionComponent />
-      
+      <FunctionComponent name = "Ashish" age = {23} author= {author} />
+      {/* <FunctionComponent />   */}
     </div>
   );
 }
--- first-react-app/src/components/FunctionComponent.jsx
+++ first-react-app/src/components/FunctionComponent.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState} from "react";
 
 
 // const  FunctionComponent = () => {}    both are  same 
-function FunctionComponent(){
+function FunctionComponent(props) {
+    const[ count, setCount]= useState(0);
+    const reduceCount = () => {
+        setCount(count - 2)
+    };
     return(
         <div>
             <p>
-                This is functional conmponent 
+                This is functional conmponent <br />
             </p>
+                <button onClick={() => setCount(count + 1)}>click me to increase by 1 </button><br />
+                <button onClick={reduceCount}>click me to decrease by 2 </button>
+                <h2>{count}</h2>
+                <h5>My fisrt name : {props.name}</h5>
+                <h5>My Age : {props.age}</h5>
+                <h5>Author name : {props.author}</h5>
         </div>
     )
 }
