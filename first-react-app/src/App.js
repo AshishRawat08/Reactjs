d5cd841574743aded039fa8040bc4e6b1f18a99c
AshishRawat08 <rawashu18@gmail.com>
2023-12-21 11:38:19 +0530

    input props

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import ClassComponent from "./components/ClassComponent";
 import FunctionComponent from "./components/FunctionComponent";
+import { useState } from "react";
 
 function App() {
-  const author = "ar";
+  const author = "William";
+  const [ name, setName] = useState("Ashish");
   return (
     <div className="App">
       <p>Class Components</p>
       <ClassComponent />
       <br />
       <p>Function Components</p>
-      <FunctionComponent name = "Ashish" age = {23} author= {author} />
-      {/* <FunctionComponent />   */}
+      {/* <FunctionComponent name = {"Ashish"} age = {23} author= {author} /> */}
+      <FunctionComponent name = {name}  age = {23} author= {author} setName = {setName}/>
     </div>
   );
 }
--- first-react-app/src/components/FunctionComponent.jsx
+++ first-react-app/src/components/FunctionComponent.jsx
@@ -1,9 +1,14 @@
 import React, { useState} from "react";
 
 
-// const  FunctionComponent = () => {}    both are  same 
+// const  FunctionComponent = () => {}      both are same 
 function FunctionComponent(props) {
+    const [ changeName, setChangeName ] = useState("");
+
+    const [ myname, setMyname] = useState();
+
     const[ count, setCount]= useState(0);
+    
     const reduceCount = () => {
         setCount(count - 2)
     };
@@ -15,10 +20,22 @@ function FunctionComponent(props) {
                 <button onClick={() => setCount(count + 1)}>click me to increase by 1 </button><br />
                 <button onClick={reduceCount}>click me to decrease by 2 </button>
                 <h2>{count}</h2>
-                <h5>My fisrt name : {props.name}</h5>
-                <h5>My Age : {props.age}</h5>
-                <h5>Author name : {props.author}</h5>
+
+                <button onClick={() => setMyname("DEV")}>click me to print my name </button><br />
+                <h1>{ myname }</h1>
+
+                 {/* PROPS ----getting name, age and author data from parent componentt(app.js) */}
+                <h3>My Fisrt name : {props.name}</h3>
+                <h3>My Age : {props.age}</h3>
+                <h3>Author name : {props.author}</h3>
+                <button onClick={() => props.setName("Akash")}>click me to change name </button><br /><br />
+
+                {/* input   */}
+                <input onChange = {(e) => setChangeName(e.target.value)}/>
+                <button onClick={() => props.setName(changeName)}>click change name to</button><br /><br />
+                
         </div>
     )
 }
+
 export default FunctionComponent;
